257d553258e2ac1b99bbd4e1e18812eff3b060d3
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-18 21:28:30 -0500

    add about view and logo pic

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+class Ferret:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+ferrets = [
+  Ferret('Lolo', 'tabby', 'Kinda rude.', 3),
+  Ferret('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Ferret('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Ferret('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
 
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello Ferret Loving Friend!</h1>')
+
+
+def about(request):
+  return render(request, 'about.html')
+
+
+def ferrets_index(request):
+  return render(request, 'ferrets/index.html', {'ferrets': ferrets})
