28a239abbf22c483930ef35608b63ec2603f94ca
MrJanHorak <janny_b_goode@hotmail.com>
2021-11-22 23:54:10 -0500

    fix uploadfilename display and tweak nav bar

--- main_app/static/js/ferret-detail.js
+++ main_app/static/js/ferret-detail.js
@@ -18,10 +18,10 @@ const fileInput = document.getElementById('file-input')
 const fileName = document.getElementById('file-name')
 
 fileInput.addEventListener('change', evt => {
-  const fileToUpload = evt.target.files[0].fileName
+  const fileToUpload = evt.target.files[0].name
   if(fileToUpload) {
     fileName.innerText = fileToUpload
   } else {
     fileName.innerText = ""
   }
-  })
+})
